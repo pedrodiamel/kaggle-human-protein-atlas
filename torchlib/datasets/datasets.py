a251eaec24b7ed2f1f6269521a3ca19487e2b387
pedrodiamel <pedrodiamel@gmail.com>
2018-12-16 02:16:32 -0300

    update dataset

--- torchlib/datasets/datasets.py
+++ torchlib/datasets/datasets.py
@@ -14,6 +14,37 @@ class ATLASDataset(object):
     Management for Human Protein Atlas dataset
     '''
 
+    idx_to_class = {
+        0:  "Nucleoplasm",  
+        1:  "Nuclear membrane",   
+        2:  "Nucleoli",   
+        3:  "Nucleoli fibrillar center",   
+        4:  "Nuclear speckles",
+        5:  "Nuclear bodies",   
+        6:  "Endoplasmic reticulum",   
+        7:  "Golgi apparatus",   
+        8:  "Peroxisomes",   
+        9:  "Endosomes",   
+        10:  "Lysosomes",   
+        11:  "Intermediate filaments",   
+        12:  "Actin filaments",   
+        13:  "Focal adhesion sites",   
+        14:  "Microtubules",   
+        15:  "Microtubule ends",   
+        16:  "Cytokinetic bridge",   
+        17:  "Mitotic spindle",   
+        18:  "Microtubule organizing center",   
+        19:  "Centrosome",   
+        20:  "Lipid droplets",   
+        21:  "Plasma membrane",   
+        22:  "Cell junctions",   
+        23:  "Mitochondria",   
+        24:  "Aggresome",   
+        25:  "Cytosol",   
+        26:  "Cytoplasmic bodies",   
+        27:  "Rods & rings"
+    }
+
     def __init__(self, 
         path,   
         train=True,
