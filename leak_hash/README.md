c5daf786f242e24ed3d2c567d2b0b2a8dbbce268
heitorrapela <hrm@cin.ufpe.br>
2019-01-07 11:30:03 -0300

    Add leak analysis

---
+++ leak_hash/README.md
@@ -0,0 +1,13 @@
+## Credits
+
+We give the credits to this guys that study hard train and test images:
+
+https://github.com/MotorCityCobra/face_ripper_9000/blob/master/duplicate_image_finder.ipynb
+https://www.kaggle.com/c/human-protein-atlas-image-classification/discussion/72534
+https://www.kaggle.com/c/human-protein-atlas-image-classification/discussion/69594
+
+Credits to these guys:
+
+https://www.kaggle.com/tilii7
+https://www.kaggle.com/tomomimoriyama
+https://www.kaggle.com/ldm314
---
+++ leak_hash/download_lili_sim/download_Tilii_hash_sim.sh
@@ -0,0 +1,6 @@
+# similiar images in train and test phase
+# https://www.kaggle.com/c/human-protein-atlas-image-classification/discussion/72534
+wget https://storage.googleapis.com/kaggle-forum-message-attachments/426936/10712/train-identical-ahash.txt
+wget https://storage.googleapis.com/kaggle-forum-message-attachments/426936/10713/train-identical-phash.txt
+wget https://storage.googleapis.com/kaggle-forum-message-attachments/426936/10711/test-identical-phash.txt
+wget https://storage.googleapis.com/kaggle-forum-message-attachments/426936/10710/test-identical-ahash.txt
